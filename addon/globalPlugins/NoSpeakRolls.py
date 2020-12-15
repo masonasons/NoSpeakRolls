362382b087bea3241bdef51bd90b46a9b0be8649
Ty <60036684+TyGillespie@users.noreply.github.com>
2020-12-15 12:30:45 -0700

    Made slightly more pep-8-ish

--- addon/globalPlugins/NoSpeakRolls.py
+++ addon/globalPlugins/NoSpeakRolls.py
@@ -7,8 +7,8 @@ import config
 import speech
 import controlTypes
 import sayAllHandler
-class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self, *args, **kwargs):
 		super(GlobalPlugin, self).__init__(*args, **kwargs)
 		# Hook to keep NVDA from announcing roles.
@@ -18,7 +18,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def _hook_getSpeechTextForProperties(self, reason=NVDAObjects.controlTypes.REASON_QUERY, *args, **kwargs):
 		role = kwargs.get('role', None)
 		if role:
-				#NVDA will not announce roles if we put it in as _role.
+			#NVDA will not announce roles if we put it in as _role.
 			kwargs['_role'] = kwargs['role']
 			del kwargs['role']
 		return self._NVDA_getSpeechTextForProperties(reason, *args, **kwargs)
